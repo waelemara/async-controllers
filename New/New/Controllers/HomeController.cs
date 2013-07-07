d63ca8cba7489e02ca780a785a73c66386562959
wael.emara <wael.emara@readify.net>
2013-07-08 09:53:25 +1000

    Async in C# 4.5

---
+++ New/New/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System; 
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace New.Controllers
+{
+    
+    
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public async Task<ActionResult> Index()
+        {
+            var faceBookCount = await GetFacebookCommentCountAsync();
+            var commentCount = await GetFacebookLikeCountAsync();
+            var faceBookStatus = new FaceBookStatus()
+                {
+                    FaceBookCount = faceBookCount,
+                    CommentCount = commentCount
+                };
+
+            return View(faceBookStatus);
+        }
+
+        public Task<int> GetFacebookCommentCountAsync()
+        {
+            
+            return Task<int>.Factory.StartNew(() =>
+                {
+                    Thread.Sleep(2000);
+                    return 20;
+                });
+        }
+
+        public Task<int> GetFacebookLikeCountAsync()
+        {
+            return Task<int>.Factory.StartNew(() =>
+                {
+                    Thread.Sleep(2000);
+                    return 25;
+                });
+        }
+
+    }
+
+    public class FaceBookStatus
+    {
+        public int FaceBookCount { get; set; }
+
+        public int CommentCount { get; set; }
+    }
+}
