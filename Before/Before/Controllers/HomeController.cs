72b3046250413accc09ca216d5a200c09f2cf883
wael.emara <wael.emara@readify.net>
2013-07-08 08:09:17 +1000

    Old Async Controllers before .net 4.5

---
+++ Before/Before/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Before.Controllers
+{
+    public class HomeController : AsyncController
+    {
+        private readonly object _lock = new object();
+        public void IndexAsync()
+        {
+            AsyncManager.OutstandingOperations.Increment(2);
+            GetFacebookLikes();
+            GetFaceBookCommentsCount();
+
+        }
+        public ActionResult IndexCompleted(string facebookLikes, string commentsCount)
+        {
+            ViewData["FacebookLikes"] = facebookLikes;
+            ViewData["CommentsCount"] = commentsCount;
+            return View("Index");
+        }
+        void GetFacebookLikes()
+        {
+            lock (_lock)
+            {
+                Thread.Sleep(2000);
+                AsyncManager.Parameters["FacebookLikes"] = "15";
+            }
+            //last line of this method
+
+            AsyncManager.OutstandingOperations.Decrement();
+        }
+        void GetFaceBookCommentsCount()
+        {
+            lock (_lock)
+            {
+                Thread.Sleep(2000);
+                AsyncManager.Parameters["CommentsCount"] = "20";
+            }
+            //last line of this method
+
+            AsyncManager.OutstandingOperations.Decrement();
+        }
+    }
+}
---
+++ Before/Before/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Before.MvcApplication" Language="C#" %>
